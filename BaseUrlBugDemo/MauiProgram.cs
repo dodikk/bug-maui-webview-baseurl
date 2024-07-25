a73ef05b07c03cb3f52fe992f91e99c89b29d0ea
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2024-07-25 20:27:42 +0300

    [maui #19792] reg custom webview handler for ios

--- BaseUrlBugDemo/MauiProgram.cs
+++ BaseUrlBugDemo/MauiProgram.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Hosting;
+
+#if IOS
+using BaseUrlBugDemo.Platforms.iOS.CustomWebView;
+#endif
+
 
 namespace BaseUrlBugDemo;
-
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
@@ -13,7 +18,19 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .ConfigureMauiHandlers((handlers) =>
+            {
+#if IOS
+                // https://blog.jhonatanoliveira.dev/customize-controls-with-handlers-with-net-maui
+                // https://github.com/dotnet/maui/wiki/Customizing-Controls-with-Handlers
+                // https://learn.microsoft.com/en-us/dotnet/maui/user-interface/handlers/customize?view=net-maui-8.0
+                // -
+                handlers.AddHandler(typeof(WebView), typeof(CustomWebViewHandler));
+#endif
             });
+
+
 
 #if DEBUG
         builder.Logging.AddDebug();

d69028f0c129bcb9595628bf054109531d327ca3
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2024-07-25 20:27:23 +0300

    [maui #19792] add workaround custom handler
    as suggested in reply by @smeagol75

---
+++ BaseUrlBugDemo/Platforms/iOS/CustomWebView/CustomMauiWkWebViewIos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+// ---
+// ios native frameworks
+// ---
+using Foundation;
+using CoreGraphics;
+using WebKit;
+
+
+namespace BaseUrlBugDemo.Platforms.iOS.CustomWebView;
+public class CustomMauiWkWebViewIos: MauiWKWebView, IWebViewDelegate
+{
+    public CustomMauiWkWebViewIos(WebViewHandler handler): base(handler)
+    {
+    }
+
+    public CustomMauiWkWebViewIos(
+        CGRect frame,
+        WebViewHandler handler
+    ): base(frame, handler)
+    {
+    }
+
+    public CustomMauiWkWebViewIos(
+        CGRect frame,
+        WebViewHandler handler,
+        WKWebViewConfiguration configuration
+    ): base(frame, handler, configuration)
+    {
+    }
+
+
+    void IWebViewDelegate.LoadHtml(string html, string baseUrl)
+    {
+        if (html == null)
+        {
+            return;
+        }
+
+        NSUrl normalizedBaseUrl =
+            (baseUrl == null)
+                ? new NSUrl(
+                    path: NSBundle.MainBundle.BundlePath,
+                    isDir: true)
+                : new NSUrl(urlString: baseUrl);
+
+        LoadHtmlString(
+            htmlString: html,
+            baseUrl: normalizedBaseUrl
+        );
+    }
+}
+
---
+++ BaseUrlBugDemo/Platforms/iOS/CustomWebView/CustomWebViewHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Handlers;
+using WebKit;
+
+namespace BaseUrlBugDemo.Platforms.iOS.CustomWebView;
+public class CustomWebViewHandler: WebViewHandler
+{
+    protected override WKWebView CreatePlatformView()
+    {
+        return new CustomMauiWkWebViewIos(handler: this);
+    }
+}
+
